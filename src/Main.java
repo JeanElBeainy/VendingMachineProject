ea863c38fb2132fee346b86d804d4920ed663e29
Jadfahed224 <jadfahed2005@gmail.com>
2025-02-28 21:24:34 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
+     VendingMachine vm = new VendingMachine();
+        Balance balance = new Balance();
+        Order order = new Order();
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("Balance: " + balance.getBalance() + "\n");
+            String input = scanner.nextLine();
+            if(input.isEmpty()) {
+                System.out.println("Caught an empty String. Please try again.");
+            }
+            else if(input.equals("exit")) {
+                break;
+            }
+            else if(input.equals("Admin")) {
+                System.out.print("Enter admin password: ");
+                String password = scanner.nextLine();
+                if(Helper.checkPassword(password)) {
+                    Helper.adminPrivilege(vm);
+                    }
+                else {
+                    System.out.println("Invalid password");
+                }
+            }
+            else {
+                Helper.userInput(input, vm, balance, order);
+            }
+        }
     }
 }
