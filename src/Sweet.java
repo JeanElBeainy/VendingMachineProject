091e91bacda855b8c8ac27a758f2738ede9c1f9d
Jean <197878093+JeanElBeainy@users.noreply.github.com>
2025-02-28 20:22:20 +0200

    Update Sweet.java

--- src/Sweet.java
+++ src/Sweet.java
@@ -1,4 +1,4 @@
-public class Sweet extends Snack {
+public class Sweet extends Item {
 
     public Sweet(String type, String name, double price, int quantity) {
         super(type, name, price, quantity);
