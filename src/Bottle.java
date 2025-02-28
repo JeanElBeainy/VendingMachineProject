6ed24cbb3553ddf5fc0d1e17fa8556b552e0bd8c
Jean <197878093+JeanElBeainy@users.noreply.github.com>
2025-02-28 20:21:42 +0200

    Update Bottle.java

--- src/Bottle.java
+++ src/Bottle.java
@@ -1,4 +1,4 @@
-public class Bottle extends Drink {
+public class Bottle extends Item {
     public Bottle(String type, String name, double price, int quantity) {
         super(type, name, price, quantity);
     }
