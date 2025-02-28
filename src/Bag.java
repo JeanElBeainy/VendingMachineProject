8ebe2c4cfd93f3531b4521c9f21f10c23213634d
Jean <197878093+JeanElBeainy@users.noreply.github.com>
2025-02-28 20:21:23 +0200

    Update Bag.java

--- src/Bag.java
+++ src/Bag.java
@@ -1,4 +1,4 @@
-public class Bag extends Snack {
+public class Bag extends Item {
 
     public Bag(String type, String name, double price, int quantity) {
         super(type, name, price, quantity);
